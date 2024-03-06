19d79c06703b444325663ef1cd7d6b36fc5b8f84
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-03-06 09:02:21 -0500

    Space x launches done

---
+++ src/services/spacex.ts
@@ -0,0 +1,28 @@
+import { type Doc, type APISpaceXResponse } from "../types/api";
+
+export async function getLaunchById({ id }: { id: string }) {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+    const launch = (await res.json()) as Doc
+    return launch
+}
+
+export async function getLatestLaunches() {
+    const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                    date_unix: "asc",
+                },
+                limit: 12,
+            },
+        }),
+    });
+
+    const { docs: launches } = (await res.json()) as APISpaceXResponse
+    return launches
+}
